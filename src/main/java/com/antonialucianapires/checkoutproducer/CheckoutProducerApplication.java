784ebe71f43c480f240b00b808e214c589fb17ee
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-10-30 14:10:25 -0300

    config: configuração inicial do projeto

---
+++ src/main/java/com/antonialucianapires/checkoutproducer/CheckoutProducerApplication.java
@@ -0,0 +1,13 @@
+package com.antonialucianapires.checkoutproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antonialucianapires/checkoutproducer/CheckoutProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonialucianapires.checkoutproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
