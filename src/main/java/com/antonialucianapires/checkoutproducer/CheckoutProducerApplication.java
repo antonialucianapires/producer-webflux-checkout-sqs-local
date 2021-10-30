e53b90ddf93277feec8a497722d4a4ce290b04ee
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-10-30 14:17:23 -0300

    config: configuração da aws e do sqs

--- src/main/java/com/antonialucianapires/checkoutproducer/CheckoutProducerApplication.java
+++ src/main/java/com/antonialucianapires/checkoutproducer/CheckoutProducerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckoutProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheckoutProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutProducerApplication.class, args);
+    }
 
 }
